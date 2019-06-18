16465ec441ac597aab2c4fa07d748dfbefc153f3
patryk920 <pblonkowski@wp.pl>
2019-06-18 02:27:06 +0200

    Add new files

---
+++ src/Liczby.java
@@ -0,0 +1,13 @@
+/*Napisz pętlę while, która wypisze na ekranie wszystkie liczby od 10 do 20 włącznie.
+
+ */
+public class Liczby {
+    public static void main(String[] args) {
+
+        int i = 10;
+        while (i <= 20) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
